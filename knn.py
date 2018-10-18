3fde436f87c27862616ab5789f7bb7a1b9b3a050
Chaitanya Shah <shahcg91@gmail.com>
2018-10-17 22:44:25 -0500

    Updated Knn code

--- knn.py
+++ knn.py
@@ -7,7 +7,7 @@ class Knn(object):
 
         distances = []
         for training_data in training_dataset:
-            euclidean_dist = distance.euclidean(training_data, test_data)
+            euclidean_dist = distance.euclidean(training_data[1:], test_data[1:])
             distances.append((euclidean_dist, training_data))
         
         # Sort by distances
@@ -16,21 +16,53 @@ class Knn(object):
         return [distance_data[1] for distance_data in sorted_distances[:k]]
 
     @classmethod
-    def get_response(cls, neighbours):
-        class_votes = {}
-        for x in range(len(neighbours)):
-            response = neighbours[x][-1]
-            if response in class_votes:
-                class_votes[response] += 1
-            else:
-                class_votes[response] = 1
-        sorted_votes = sorted(class_votes.iteritems(), key=lambda x: x[0], reverse=True)
+    def get_classification(cls, neighbours):
+        """ Returns label
+        """
+        class_votes = dict()
+        for neighbour in neighbours:
+            label = neighbour[0]
+            vote = class_votes.get(label,0)
+            class_votes[label] = vote + 1
+            
+        sorted_votes = sorted(
+            list(class_votes.items()), key=lambda x: x[0], reverse=True)
         return sorted_votes[0][0]
 
     @classmethod
-    def get_accuracy(cls, test_set, predictions):
+    def get_accuracy(cls, prediction_n_test):
         correct = 0
-        for x in range(len(test_set)):
-            if test_set[x][-1] is predictions[x]:
+        for prediction, test in prediction_n_test:
+            if prediction == test:
                 correct += 1
-        return (correct/float(len(test_set))) * 100.0
+        return (float(correct)/float(len(prediction_n_test))) * 100.0
+    
+
+def calculate_knn_and_accuracy(training_data, test_data, k_val):
+
+    # Get list of nearest neighbors for each test
+    prediction_n_test_list = list()
+    for test_data_instance in test_data:
+        nearest_neighbors = Knn.get_nearest_neighbors(
+            training_dataset=training_data, 
+            test_data=test_data_instance,
+            k=k_val)
+        calculated_classification = Knn.get_classification(nearest_neighbors)
+        prediction_n_test_list.append((test_data_instance[0], calculated_classification)) # Given Classification, Calculated Classification
+
+    # Calculate Accuracy
+    return Knn.get_accuracy(prediction_n_test_list)
+
+
+# Get Data
+def excute(master_dataset, k_values):
+
+    master_data = master_dataset
+    training_data = master_data[:-100]
+    test_data = master_data[-100:]
+
+    k_values = [1, 5, 10, 30]
+
+    for k_val in k_values:
+        accuracy = calculate_knn_and_accuracy(training_data, test_data, k_val)
+        print("K:",k_val, "Accuracy:", accuracy)
