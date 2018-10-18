4c31c0bcc83d67363ed364fb0f3f6db72638b595
Jinesh Modi <jinesh1994@gmail.com>
2018-10-17 22:16:07 -0500

    Merged changes

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from copy import deepcopy
 
 def main():
     try:
-        data = np.genfromtxt("NBAstats.csv", delimiter=",", skip_header=1, usecols=(1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28))
+        data = np.genfromtxt("NBAstats.csv", delimiter=",", skip_header=1, usecols=(
+        1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28))
         knn_data = pd.read_csv("NBAstats.csv", delimiter=",")
         data = pd.DataFrame(data=data)
         position_of_player = knn_data['Pos']
